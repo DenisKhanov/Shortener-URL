e2a373882bc67adb7eb433b55c004897210fd460
DK <denis_hanov@hotmail.com>
2023-10-06 14:54:07 +0300

    Добавил обработку ошибок в handler

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -11,23 +11,30 @@ func Url(w http.ResponseWriter, r *http.Request) {
 		url, _ := io.ReadAll(r.Body)
 		if len(url) == 0 {
 			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("Ссылка в теле не передана"))
-		} else {
-			shortURL := GetShortURL(string(url))
-			w.WriteHeader(http.StatusCreated)
-			w.Header().Set("Content-Type", "text/plain")
-			w.Write([]byte(shortURL))
-		}
-	}
-	if r.Method == http.MethodGet {
-		path := r.URL.Path[1:]
-		originURL, exists := GetOriginURL(path)
-		if !exists {
-			http.NotFound(w, r)
-		} else {
-			w.Header().Set("Location", originURL)
-			w.WriteHeader(http.StatusTemporaryRedirect)
-		}
-	}
+			_, err := w.Write([]byte("Ссылка в теле не передана"))
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			} else {
+				shortURL := GetShortURL(string(url))
+				w.WriteHeader(http.StatusCreated)
+				w.Header().Set("Content-Type", "text/plain")
+				_, err := w.Write([]byte(shortURL))
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
 
+				}
+			}
+			if r.Method == http.MethodGet {
+				path := r.URL.Path[1:]
+				originURL, exists := GetOriginURL(path)
+				if !exists {
+					http.NotFound(w, r)
+				} else {
+					w.Header().Set("Location", originURL)
+					w.WriteHeader(http.StatusTemporaryRedirect)
+				}
+			}
+
+		}
+	}
 }
