832e398974ed59b923f2205caa6649e3bdca59b9
DK <denis_hanov@hotmail.com>
2023-10-12 20:10:18 +0300

    Подправил тесты Конфига

--- internal/app/config/config_test.go
+++ internal/app/config/config_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
@@ -19,14 +20,19 @@ func TestNewConfig(t *testing.T) {
 			expected: &ENVConfig{EnvServAdr: "localhost:8080", EnvBaseURL: "http://localhost:8080"},
 		},
 		{
+			args:     []string{"cmd", "-a", "localhost:9090", "-b", "http://test.com"},
+			expected: &ENVConfig{EnvServAdr: "localhost:9090", EnvBaseURL: "http://test.com"},
+		},
+		{
 			envServAddr: "localhost:9090",
 			envBaseURL:  "http://example.com",
 			args:        []string{"cmd", "-a", "localhost:7070", "-b", "http://test.com"},
-			expected:    &ENVConfig{EnvServAdr: "localhost:7070", EnvBaseURL: "http://test.com"},
+			expected:    &ENVConfig{EnvServAdr: "localhost:9090", EnvBaseURL: "http://example.com"},
 		},
 	}
 
 	for _, test := range tests {
+
 		if test.envServAddr != "" {
 			os.Setenv("SERVER_ADDRESS", test.envServAddr)
 		} else {
@@ -42,6 +48,7 @@ func TestNewConfig(t *testing.T) {
 		os.Args = test.args
 		defer func() { os.Args = oldArgs }()
 
+		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError) // Сбрасываем значение флагов перед каждым тестом
 		cfg := NewConfig()
 
 		assert.Equal(t, test.expected, cfg)
